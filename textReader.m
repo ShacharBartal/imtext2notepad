14d12a47ee75bcfd8e246f68e43dd7ca3e203d1c
Adam <gilnimrodnevo@gmail.com>
2019-03-09 13:06:38 +0200

    comments, final

--- textReader.m
+++ textReader.m
@@ -1,9 +1,11 @@
+%reading our database picture.
 database = imread('database_Ariel20_pic.docx.png');
+
 databaseBW=imbinarize(database); %making pic binary.
 databaseBW=databaseBW(:,:,1);     %making pic one dimentional.
 databaseBWinv=1.-databaseBW;      %inverting black and white.
 databaseBWlabel=bwlabel(databaseBWinv); %labeling all letters
-databaseBWlabel=comb(databaseBWlabel);  %calling our function that combines
+databaseBWlabel=comb(databaseBWlabel);  %calling our function that manually combines
 %2 parts of the same letter.
 
 %running dictionary
@@ -17,69 +19,57 @@ Datacell=cell([70 3]);
 
 for k=1 : length(bboxes)
     currBB = bboxes(k).BoundingBox;
+    %extracting matrix of a letter from the big picture of all letters.
     A=databaseBWlabel(int64(currBB(2)):int64(currBB(2)+currBB(4)) ,int64(currBB(1)):int64(currBB(1)+currBB(3)));
     
     
-    
+    % resizing and cleaning noise
     A = A*100;
     A = imresize(int32(A), [100,100]);
-  %  A = medfilt2(A);
+    A = medfilt2(A);
     for i=1:100
         for j=1:100
              if A(i,j) >= k*50
-           % if A(i,j) >= 99
-           % if A(i,j) > 0
               A(i,j) = 1;
             else
                A(i,j) = 0;
             end
         end
      end
- %   A=imopen(A,strel('disk',5));
+
     B=DictionaryCell(k, :);
     Datacell(k,:)={A,size(A),B(2)};
 end
-       % ex =imread('database_Ariel20_pic.docx.png');
-      %  exBW=imbinarize(ex);        % making pic binary.
-      %  exBW=exBW(:,:,1);           % making pic one dimentional.
-      %  exBWinv=1.-exBW;            % inverting black and white.
-       % exBWlabel=bwlabel(exBWinv); % labeling all letters
-
+    
         
-        
-%Excell will hold matrix of each letter.
-
-
+%Excell will hold matrix of each letter from input image.
 bboxes = regionprops(exBWlabel,'BoundingBox');
 centers= regionprops(exBWlabel,'Centroid');
 exBWlabel=int64(exBWlabel);
-Excell=cell([length(bboxes) 5]);
+Excell=cell([length(bboxes) 4]);
 
-%finding range of line by using first letter range
+%finding range of line by using first letter height.
 currBB = bboxes(1).BoundingBox;
 cent= centers(1).Centroid;
-%range= currBB(4)/4*3;
 range= currBB(4);
 
-
+%now we find where each letter is positioned in each line.
 firstYpos=round(cent);
 nextYpos=[0 0];
-
 outOfRange=0;
 
 for k=1 : length(bboxes)
     currBB = bboxes(k).BoundingBox;
     currCenter=centers(k).Centroid;
+    %extracting matrix of a letter from the big input image.
     A=exBWlabel(int64(currBB(2)):int64(currBB(2)+currBB(4)) ,int64(currBB(1)):int64(currBB(1)+currBB(3)));
    
+    % resizing and cleaning noise
     A = A*100;
-    
     A = imresize(int32(A), [100,100]);
-  %   A = medfilt2(A);
+    A = medfilt2(A);
     for i=1:100
         for j=1:100
-           % if A(i,j) >= k*25
-           %if A(i,j) >= 99
             if A(i,j) > 0
                 A(i,j) = 1;
             else
@@ -87,23 +77,24 @@ for k=1 : length(bboxes)
             end
         end
     end
-   % A=imopen(A,strel('disk',5));
-    %B=DictionaryCell(k, :);
-    
-    temp = A;
+   
+    %cheking to see in each iterarion if the y position is in range of the
+    %line if so we fix the y position to be that of the line.
+    %else we keep the last position that is out of the line to make a new
+    %line standart.
     if currCenter(2)-range < firstYpos(2) && currCenter(2)+range > firstYpos(2)
-    Excell(k,:)={A,size(A),round(currCenter(1)),firstYpos(2), round(currCenter(1))+ 1000* firstYpos(2)};
+    Excell(k,:)={A,size(A),round(currCenter(1)),firstYpos(2)};
     else
-        Excell(k,:)={A,size(A),round(currCenter(1)),round(currCenter(2)),round(currCenter(1))+ 1000* firstYpos(2)};
+        Excell(k,:)={A,size(A),round(currCenter(1)),round(currCenter(2))};
         outOfRange= outOfRange+1;  
         nextYpos=round(currCenter);
-    end
-
-          
+    end       
 end
 
+%pastY will hold all the lines that we found and will find.
 pastY = firstYpos;
 
+%finding all the lines and fixing the characters positions.
 while (outOfRange >1)
     
     outOfRange = 0;
@@ -111,47 +102,47 @@ while (outOfRange >1)
     addToPastY=0;
     for k=1 : length(bboxes)
 
-       % currBB = bboxes(k).BoundingBox;
-       % currCenter=centers(k).Centroid;
        currCenter(1)=Excell{k,3};
        currCenter(2)=Excell{k,4};
         
         goIn=1;
     
+        %checking to see the value is not already a legitimate line value.
     for i=1 : length(pastY(:,1))
         if (round(currCenter(2)) == pastY(i,2))
             goIn = 0;
         end
     end
     
+    
+    %if there is a line in which we can fix the char position to
      if goIn == 1
             if currCenter(2)-range < firstYpos(2) && currCenter(2)+range > firstYpos(2) 
               Excell(k,3)={round(currCenter(1))};
               Excell(k,4)={round(firstYpos(2))};
-              Excell(k,5)={round(firstYpos(2))*1000+round(currCenter(1))};
+       %else add a new line standart.       
             else
                 
                 addToPastY = 1;
                 nextYpos=round(currCenter);
-                % Excell(k,:)={A,size(A),round(currCenter)};
                 outOfRange= outOfRange+1;    
             end    
     end
     end
+    %adding the new line position to all the lines.
     if addToPastY == 1
         pastY = [pastY; nextYpos];
         addToPastY=0;
     end
 end
 
-
-
-%the main check
+%sorting by lines and x position
 Excell_sorted = sortrows(Excell,[4 3]);
 
-%finding spaceLength
+%finding spaceLength by finding the average range between letters.
 sum=0;
 divide_by=1;
+
 for k=1 : length(Excell_sorted)-1
     if Excell_sorted{k,3}<Excell_sorted{k+1,3}
         sum=sum+ Excell_sorted{k+1,3} - Excell_sorted{k,3};
@@ -162,8 +153,8 @@ end
 spaceLength=sum/divide_by;
 spaceLength=spaceLength*1.3;
 
+%the main check, checking pixelwise which letter fits the most.
 indexToWrite=1;
-
 
 for k=1 : length(Excell_sorted)
     curLetterToWrite = Datacell(1,3);
@@ -197,6 +188,7 @@ for k=1 : length(Excell_sorted)
        end
     end
     
+    %creating our final string, the printing takes place at imtext2notpad.m
     str(indexToWrite)=curLetterToWrite(1);           
     indexToWrite = indexToWrite+1;
     
@@ -213,9 +205,11 @@ for k=1 : length(Excell_sorted)
     end
 end
 
-% a function that combines 2 part letter together like in "i"
-% combines the dot with the line
 
+
+
+% a function that combines 2 part letter together like in "i"
+% combines the dot with the line.
 function database = comb(database)
 [row, col] = size(database);
 for r=1:row
@@ -226,9 +220,7 @@ for r=1:row
         if database(r,c)==19
             database(r,c)=17;
         end
-       % if database(r,c)==56
-       %     database(r,c)=0;
-       % end
+    
         if database(r,c)==58
             database(r,c)=57;
         end
